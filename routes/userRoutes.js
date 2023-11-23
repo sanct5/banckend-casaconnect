796a1b0d306221a8432a82b2426dbbc05af1c9eb
sanct5 <130994767+sanct5@users.noreply.github.com>
2023-11-23 05:00:12 -0500

    configiracion de ruta post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator'); 
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/authController.js');
-const {listarUsuarios} = require('../controllers/usersController.js');
+const { listarUsuarios } = require('../controllers/usersController.js');
 const { validarCampos } = require('../middlewares/validar-campos.js');
-const { validarJWT } = require('../middlewares/validar-token.js')
+const { validarJWT } = require('../middlewares/validar-token.js');
 
-router.post('/', loginUsuario)
+router.post('/login', loginUsuario)
 
 router.post(
-    '/new', 
+    '/new',
     [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', "la clave debe tener al menos 6 digitos").isLength({ min: 6}),
+        check('password', "la clave debe tener al menos 6 digitos").isLength({ min: 6 }),
         validarCampos
     ],
-    crearUsuario 
+    crearUsuario
 )
 
 router.get('/renew', validarJWT, revalidarToken)
